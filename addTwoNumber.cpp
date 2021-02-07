a5bd9e6ce76a9a1afa098c49c76c5e77792614f7
Harsh Mangla <harsh1234.cse18@chitkara.edu.in>
2021-02-07 21:54:35 +0530

    Add two numbers using variables.

---
+++ addTwoNumber.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    cout << "\n\n Print the sum of two numbers :\n";
+	cout << "-----------------------------------\n";
+	int a;
+	int b;
+	int sum;
+	a=29;
+	b=30;
+	sum=a+b;
+	cout << " The sum of "<< a << " and "<<b <<" is : "<< sum <<"\n\n" ;
+}
